6f0ddfb1bf6b87ed99cdecabe9bff2eceb2984af
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-10-26 19:27:38 +0000

    Automatically backed up by Learn

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,8 @@
 class Bird < ApplicationRecord
+<<<<<<< HEAD
     has_many :sightings
     has_many :locations, through: :sightings 
+=======
+  belongs_to :location
+>>>>>>> 139f2580df11b0c8a834b7afb38e4c504e522448
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ApplicationRecord
+<<<<<<< HEAD
   has_many :sightings 
   has_many :birds, through: :sightings 
+=======
+  has_many :birds
+>>>>>>> 139f2580df11b0c8a834b7afb38e4c504e522448
 end
